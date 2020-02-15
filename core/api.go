fb94c0f9c36923a94ba4d57c1766b5e4b0b14fd9
Akram <900658008.akram@gmail.com>
2020-02-15 14:59:53 +0500

    123

--- core/api.go
+++ core/api.go
@@ -1,7 +1,429 @@
 package core
 
-import "fmt"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"strings"
+)
 
-func Opp()  {
-	fmt.Println("daas")
+////-----------Structs-------------------------
+type Client struct {
+	Id         int
+	Login      string
+	Password   string
+	Name       string
+	Surname    string
+	SerialPass string
+	Phone      int
+	MainAcc    int
+}
+
+type Accounts struct {
+	Id       int
+	Name     string
+	Number   int
+	Money    int
+	ClientId int
+}
+
+type Services struct {
+	Id            int
+	Name          string
+	IdPayment     string
+	AccountNumber int
+}
+
+type Terminals struct {
+	Id      int
+	Number  string
+	Address string
+}
+
+//----------------------------------------------
+
+func Init(db *sql.DB) (err error) {
+	ddls := []string{managersDDL, managersInitialData, clientsDDL, accountsDDL, initAccountDDL, terminalsDDL, servicesDDL}
+	for _, ddl := range ddls {
+		_, err = db.Exec(ddl)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Login(login, password, nameTable string, db *sql.DB) (bool, error) {
+	var dbLogin, dbPassword string
+
+	if nameTable == "managers" {
+		loginSQL = loginManSQL
+	}
+	if nameTable == "clients" {
+		loginSQL = loginCliSQL
+	}
+	err := db.QueryRow(loginSQL, login).Scan(&dbLogin, &dbPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	if dbPassword != password {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func AddTerminals(db *sql.DB, number, address string) error {
+	var idTerminal int
+	err := db.QueryRow(exitsTerminalSQL, number).Scan(&idTerminal)
+	if err == nil {
+		return errors.New("Terminal already exits")
+	} else if err == sql.ErrNoRows {
+		_, err = db.Exec(insertTerminal, number, address)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return err
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+func AddServices(db *sql.DB, name string, idPayment string) error {
+	name = strings.Trim(name, "\n")
+	idPayment = strings.Trim(idPayment, "\n")
+
+	var idService int
+	err := db.QueryRow(exitsServicesSQL, name).Scan(&idService)
+	if err == nil {
+		return errors.New("Service already exits")
+	} else if err == sql.ErrNoRows {
+		var numberLastAcc int
+		err := db.QueryRow(lastAccountId).Scan(&numberLastAcc)
+		if err != nil {
+			return err
+		}
+		numberLastAcc++
+
+		_, err = db.Exec(insertAccountSQL, name, numberLastAcc, 0, 0)
+		if err != nil {
+			return err
+		}
+
+		_, err = db.Exec(insertService, name, idPayment, numberLastAcc)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+	return err
+}
+
+func AddNewClient(db *sql.DB, newClinet Client) (int, error) {
+	var numberLastAcc, clientId int
+	err := db.QueryRow(lastAccountId).Scan(&numberLastAcc)
+	if err != nil {
+		return 0, err
+	}
+	numberLastAcc++
+
+	_, err = db.Exec(insertClientSQL, newClinet.Login, newClinet.Password, newClinet.Name, newClinet.Surname, newClinet.SerialPass, newClinet.Phone, numberLastAcc)
+	if err != nil {
+		return 0, err
+	}
+	err = db.QueryRow(lastClientId, newClinet.Login).Scan(&clientId)
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = db.Exec(insertAccountSQL, "Alif", numberLastAcc, 0, clientId)
+	if err != nil {
+		return 0, err
+	}
+
+	return numberLastAcc, nil
+}
+
+func AddAccountByLogin(db *sql.DB, login, name string) (int, error) {
+	var clientId int
+	err := db.QueryRow(idClientByLogin, login).Scan(&clientId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 0, errors.New("Login was not detected!")
+		}
+		return 0, err
+	}
+
+	//-----------------------------------------------------
+	var numberLastAcc int
+	err = db.QueryRow(lastAccountId).Scan(&numberLastAcc)
+	if err != nil {
+		return 0, err
+	}
+	numberLastAcc++
+	_, err = db.Exec(insertAccountSQL, name, numberLastAcc, 0, clientId)
+	if err != nil {
+		return 0, err
+	}
+	//-----------------------------------------------------
+
+	return numberLastAcc, nil
+}
+
+func AddAccountByPhone(db *sql.DB, phone, name string) (int, error) {
+	var clientId int
+	err := db.QueryRow(idClientByPhone, phone).Scan(&clientId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 0, errors.New("Login was not detected!")
+		}
+		return 0, err
+	}
+
+	//-----------------------------------------------------
+	var numberLastAcc int
+	err = db.QueryRow(lastAccountId).Scan(&numberLastAcc)
+	if err != nil {
+		return 0, err
+	}
+	numberLastAcc++
+	_, err = db.Exec(insertAccountSQL, name, numberLastAcc, 0, clientId)
+
+	if err != nil {
+		return 0, err
+	}
+	//-----------------------------------------------------
+
+	return numberLastAcc, nil
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+func ShowAccounts(db *sql.DB, accounts *[]Accounts) error {
+	rows, err := db.Query(allAccounts)
+	if err != nil {
+		return err
+	}
+	for rows.Next() {
+		var res Accounts
+		err = rows.Scan(&res.Id, &res.Name, &res.Number, &res.Money, &res.ClientId)
+		if err != nil {
+			return err
+		}
+		*accounts = append(*accounts, res)
+	}
+	return nil
+}
+
+func ShowTerminals(db *sql.DB, terminal *[]Terminals) error {
+	rows, err := db.Query(allTerminalSQL)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	var number, address string
+	var id int
+	for rows.Next() {
+		err = rows.Scan(&id, &number, &address)
+		*terminal = append(*terminal, Terminals{id, number, address})
+		if err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
+
+func ShowClients(db *sql.DB, clients *[]Client) error {
+	rows, err := db.Query(allClients)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		var res Client
+		err = rows.Scan(&res.Id, &res.Login, &res.Password, &res.Name, &res.Surname, &res.SerialPass, &res.Phone, &res.MainAcc)
+		if err != nil {
+			return err
+		}
+		*clients = append(*clients, res)
+	}
+	if rows.Err() != nil {
+		return rows.Err()
+	}
+	return nil
+}
+
+func UpdateAccounts(db *sql.DB, accounts *[]Accounts) error {
+	//ddls := []string{managersDDL,managersInitialData,clientsDDL,accountsDDL,initAccountDDL,terminalsDDL,servicesDDL}
+	_, err := db.Exec(accountsDDL)
+	if err != nil {
+		return err
+	}
+	for _, val := range *accounts {
+		//-----------:id, :name, :number, :money, :client_id
+		_, err := db.Exec(updateAccount, val.Id, val.Name, val.Number, val.Money, val.ClientId)
+		fmt.Printf(updateAccount, val.Id, val.Name, val.Number, val.Money, val.ClientId)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func UpdateTerminals(db *sql.DB, terminals *[]Terminals) error {
+	//ddls := []string{managersDDL,managersInitialData,clientsDDL,accountsDDL,initAccountDDL,terminalsDDL,servicesDDL}
+	_, err := db.Exec(terminalsDDL)
+	if err != nil {
+		return err
+	}
+	for _, val := range *terminals {
+		//-----------(:id, :number, :address)
+		_, err := db.Exec(updateTerminals, val.Id, val.Number, val.Address)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func UpdateClients(db *sql.DB, clients *[]Client) error {
+	//ddls := []string{managersDDL,managersInitialData,clientsDDL,accountsDDL,initAccountDDL,terminalsDDL,servicesDDL}
+	_, err := db.Exec(clientsDDL)
+	if err != nil {
+		return err
+	}
+	for _, val := range *clients {
+		//-----------(:id, :login, :password, :name, :surname, :serialPass, :phone, :mainAcc)
+		_, err := db.Exec(updateClients, val.Id, val.Login, val.Password, val.Name, val.Surname, val.SerialPass, val.Phone, val.MainAcc)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+func ShowServices(db *sql.DB, services *[]Services) error {
+	rows, err := db.Query(allServices)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		//--------------------Id,Name,Payment,AccountNumber
+		var res Services
+		err = rows.Scan(&res.Id, &res.Name, &res.IdPayment, &res.AccountNumber)
+		if err != nil {
+			return err
+		}
+		*services = append(*services, res)
+	}
+	if rows.Err() != nil {
+		return rows.Err()
+	}
+	return nil
+}
+
+func ShowAccountById(db *sql.DB, accounts *[]Accounts, mainAcc *int, id int) error {
+	rows, err := db.Query(allAccountsById, id)
+	if err != nil {
+		return err
+	}
+	var mainAccount int
+	err = db.QueryRow(mainAccountSqlById, id).Scan(&mainAccount)
+	*mainAcc = mainAccount
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		var res Accounts
+		err = rows.Scan(&res.Id, &res.Name, &res.Number, &res.Money, &res.ClientId)
+		if err != nil {
+			return err
+		}
+		*accounts = append(*accounts, res)
+	}
+	return nil
+}
+
+func IdClientByLogin(db *sql.DB, login string) (int, error) {
+	var clientId int
+	err := db.QueryRow(idClientByLogin, login).Scan(&clientId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 0, errors.New("Login was not detected!")
+		}
+		return 0, err
+	}
+	return clientId, nil
+}
+
+func IdClientByAccount(db *sql.DB, numberAcc int) (int, error) {
+	var clientId int
+	err := db.QueryRow(idClientByAccount, numberAcc).Scan(&clientId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 0, errors.New("Login was not detected!")
+		}
+		return 0, err
+	}
+	return clientId, nil
+}
+
+func NumberAccountByPhone(db *sql.DB, phone int) (int, error) {
+	var numberAccount int
+	err := db.QueryRow(numberAccountByPhone, phone).Scan(&numberAccount)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 0, errors.New("Undefined phone")
+		}
+		return 0, errors.New("Server problem")
+	}
+
+	return numberAccount, nil
+}
+
+func CheckAccount(db *sql.DB, numberAcc int) error {
+	var id int
+	err := db.QueryRow(checkAccount, numberAcc).Scan(&id)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return errors.New("Undefined number account")
+		}
+		return err
+	}
+	return nil
+}
+
+func AddMoneyAccountNumber(db *sql.DB, numberAcc int, amount int) error {
+	err := CheckAccount(db, numberAcc)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(updateBalanceAccount, amount, numberAcc)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ChangeMainAcc(db *sql.DB, clientId, accountId int) error {
+	_, err := db.Exec(updateMainAcc, accountId, clientId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- core/sql.go
+++ core/sql.go
@@ -1,1 +1,95 @@
 package core
+
+//---------------Manager
+const managersDDL = `
+CREATE TABLE IF NOT EXISTS managers
+(
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    name    TEXT    NOT NULL,
+    login   TEXT    NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);`
+const managersInitialData = `INSERT INTO managers
+VALUES (1, 'Akram', '900658008', '8008'),
+       (2, 'Azam', '985658008', '8008')
+       ON CONFLICT DO NOTHING;`
+
+//------------Client-------------------------------
+
+const clientsDDL = `
+CREATE TABLE IF NOT EXISTS clients
+(
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    login   TEXT   NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    name    TEXT    NOT NULL,
+    surname    TEXT    NOT NULL,
+	serialPass TEXT    NOT NULL UNIQUE,
+	phone INTEGER NOT NULL UNIQUE,
+	mainAcc INTEGER UNIQUE
+);`
+
+const loginManSQL = `SELECT login, password FROM managers WHERE login = ? ;`
+const loginCliSQL = `SELECT login, password FROM clients WHERE login = ? ;`
+const idClientByLogin = `SELECT id FROM clients WHERE login = ? ;`
+const idClientByAccount = `SELECT client_id FROM accounts WHERE number = ? ;`
+const idClientByPhone = `SELECT id FROM clients WHERE phone = ? ;`
+const numberAccountByPhone = `SELECT mainAcc FROM clients WHERE phone = ? ;`
+
+var loginSQL string
+
+const insertClientSQL = `INSERT INTO clients(login, password, name, surname, serialPass, phone, mainAcc)  VALUES (?, ?, ?, ?, ?, ?, ?);`
+const insertAccountSQL = `INSERT INTO accounts (name, number, money, client_id)  VALUES (?, ?, ?, ?);`
+const lastClientId = `SELECT id FROM clients WHERE login=:log;`
+
+//---------------Account------------
+const accountsDDL = `
+CREATE TABLE IF NOT EXISTS accounts
+(
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+	name   TEXT NOT NULL,
+    number INTEGER NOT NULL,
+    money INTEGER NOT NULL,
+    client_id      TEXT NOT NULL
+
+);`
+const initAccountDDL = `INSERT INTO accounts
+VALUES (1 , 'Initial', 100000000,0,0)
+       ON CONFLICT DO NOTHING;`
+const lastAccountId = `SELECT number FROM accounts ORDER BY id DESC;`
+const allAccountsById = `SELECT * FROM accounts  WHERE client_id=? ORDER BY money DESC;`
+const allAccounts = `SELECT * FROM accounts;`
+const mainAccountSqlById = `SELECT mainAcc FROM clients  WHERE id=?;`
+const updateBalanceAccount = `UPDATE accounts SET money = money + :money WHERE number=:numberAcc ;`
+const checkAccount = `SELECT id FROM accounts WHERE number= :numberAcc ;`
+const updateAccount = `INSERT INTO accounts VALUES (:id, :name, :number, :money, :client_id) ON CONFLICT DO NOTHING;`
+const updateTerminals = `INSERT INTO terminals VALUES (:id, :number, :address) ON CONFLICT DO NOTHING;`
+const updateClients = `INSERT INTO clients VALUES (:id, :login, :password, :name, :surname, :serialPass, :phone, :mainAcc) ON CONFLICT DO NOTHING;`
+
+//-------------------------Bankomat-----------------
+const terminalsDDL = `CREATE TABLE IF NOT EXISTS terminals
+(
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+	number   INTEGER NOT NULL UNIQUE,
+    address TEXT NOT NULL
+);`
+const exitsTerminalSQL = `SELECT id FROM terminals WHERE number=?;`
+const allTerminalSQL = `SELECT * FROM terminals;`
+const insertTerminal = `INSERT INTO terminals(number, address)  VALUES (?, ?);`
+const updateMainAcc = `UPDATE clients SET mainAcc = :AccId WHERE id=:clID ;`
+
+//---------------------------Uslugi
+
+const servicesDDL = `CREATE TABLE IF NOT EXISTS services
+(
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+	name   TEXT NOT NULL UNIQUE,
+    idPayment TEXT NOT NULL,
+    accountNumber INTEGER NOT NULL
+
+
+);`
+const exitsServicesSQL = `SELECT id FROM services WHERE name=?;`
+const insertService = `INSERT INTO services (name, idPayment, accountNumber)  VALUES (?, ?, ?);`
+const allServices = `SELECT * FROM services;`
+const allClients = `SELECT * FROM clients;`
